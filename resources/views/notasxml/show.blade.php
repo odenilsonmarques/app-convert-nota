c52a71c36a6c29d6d48eeb1c7b0e83fde2296ddb
odenilson marques <odmarquesgm@gmail.com>
2023-11-20 18:15:04 -0300

    change style in table

--- resources/views/notasxml/show.blade.php
+++ resources/views/notasxml/show.blade.php
@@ -1,82 +1,89 @@
 @extends('layouts.template')
-
 @section('title', 'dados da notas')
-
 @section('content')
 
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
 
-                <h1>Detalhes da Nota Fiscal</h1>
 
-                <table>
-                    <tr>
-                        <th>Informação</th>
-                        <th>Valor</th>
-                    </tr>
+                <div class="table-responsive table-notas">
+                    <table class="table table table-hover caption-top">
+                        <caption>Detalhes da Nota</caption>
+                        <thead class="table header-table">
+                            <tr>
+                                <th>Informação</th>
+                                <th>Valor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><strong>ID</strong></td>
+                                <td>{{ $xml->NFe->infNFe->ide->cNF }}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Natureza da Operação</strong></td>
+                                <td>{{ $xml->NFe->infNFe->ide->natOp }}</td>
+                            </tr>
+                            <!-- Adicione mais linhas conforme necessário para outras informações -->
 
-                    <tr>
-                        <td>ID</td>
-                        <td>{{ $xml->NFe->infNFe->ide->cNF }}</td>
-                    </tr>
-                    <tr>
-                        <td>Natureza da Operação</td>
-                        <td>{{ $xml->NFe->infNFe->ide->natOp }}</td>
-                    </tr>
-                    <!-- Adicione mais linhas conforme necessário para outras informações -->
+                            <!-- Emitente -->
+                            <tr>
+                                <td><strong>Emitente</strong></td>
+                                <td>{{ $xml->NFe->infNFe->emit->xNome }}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>CNPJ Emitente</strong></td>
+                                <td>{{ $xml->NFe->infNFe->emit->CNPJ }}</td>
+                            </tr>
+                            <!-- Adicione mais linhas conforme necessário para outras informações do emitente -->
 
-                    <!-- Emitente -->
-                    <tr>
-                        <td>Emitente</td>
-                        <td>{{ $xml->NFe->infNFe->emit->xNome }}</td>
-                    </tr>
-                    <tr>
-                        <td>CNPJ Emitente</td>
-                        <td>{{ $xml->NFe->infNFe->emit->CNPJ }}</td>
-                    </tr>
-                    <!-- Adicione mais linhas conforme necessário para outras informações do emitente -->
+                            <!-- Destinatário -->
+                            <tr>
+                                <td><strong>Destinatário</strong></td>
+                                <td>{{ $xml->NFe->infNFe->dest->xNome }}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>CPF Destinatário</strong></td>
+                                <td>{{ $xml->NFe->infNFe->dest->CPF }}</td>
+                            </tr>
+                            <!-- Adicione mais linhas conforme necessário para outras informações do destinatário -->
 
-                    <!-- Destinatário -->
-                    <tr>
-                        <td>Destinatário</td>
-                        <td>{{ $xml->NFe->infNFe->dest->xNome }}</td>
-                    </tr>
-                    <tr>
-                        <td>CPF Destinatário</td>
-                        <td>{{ $xml->NFe->infNFe->dest->CPF }}</td>
-                    </tr>
-                    <!-- Adicione mais linhas conforme necessário para outras informações do destinatário -->
+                            <!-- Detalhes do Produto -->
+                            @foreach ($xml->NFe->infNFe->det as $detalhe)
+                                <tr>
+                                    <td><strong>Produto</strong></td>
+                                    <td>{{ $detalhe->prod->xProd }}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Quantidade</strong></td>
+                                    <td>{{ $detalhe->prod->qCom }}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Valor Unitário</strong></td>
+                                    <td>{{ $detalhe->prod->vUnCom }}</td>
+                                </tr>
+                                <!-- Adicione mais linhas conforme necessário para outras informações do produto -->
+                            @endforeach
 
-                    <!-- Detalhes do Produto -->
-                    @foreach ($xml->NFe->infNFe->det as $detalhe)
-                        <tr>
-                            <td>Produto</td>
-                            <td>{{ $detalhe->prod->xProd }}</td>
-                        </tr>
-                        <tr>
-                            <td>Quantidade</td>
-                            <td>{{ $detalhe->prod->qCom }}</td>
-                        </tr>
-                        <tr>
-                            <td>Valor Unitário</td>
-                            <td>{{ $detalhe->prod->vUnCom }}</td>
-                        </tr>
-                        <!-- Adicione mais linhas conforme necessário para outras informações do produto -->
-                    @endforeach
+                            <!-- Informações Adicionais -->
+                            <tr>
+                                <td><strong>Observações Fisco</strong></td>
+                                <td>{{ $xml->NFe->infNFe->infAdic->infAdFisco }}</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Informações Complementares</strong></td>
+                                <td>{{ $xml->NFe->infNFe->infAdic->infCpl }}</td>
+                            </tr>
+                            <!-- Adicione mais linhas conforme necessário para outras informações adicionais -->
+                        </tbody>
 
-                    <!-- Informações Adicionais -->
-                    <tr>
-                        <td>Observações Fisco</td>
-                        <td>{{ $xml->NFe->infNFe->infAdic->infAdFisco }}</td>
-                    </tr>
-                    <tr>
-                        <td>Informações Complementares</td>
-                        <td>{{ $xml->NFe->infNFe->infAdic->infCpl }}</td>
-                    </tr>
-                    <!-- Adicione mais linhas conforme necessário para outras informações adicionais -->
 
-                </table>
+
+                    </table>
+
+                </div>
+
 
 
             </div>
